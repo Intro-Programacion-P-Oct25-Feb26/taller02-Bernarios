71d6f79f2ac56120dc188c0e87c7089065d130dc
danyberna2002@gmail.com <danyberna2002@gmail.com>
2025-10-30 16:28:13 -0500

    segundo commit

--- proyecto01/src/proyecto01/Proyecto01.java
+++ proyecto01/src/proyecto01/Proyecto01.java
@@ -15,6 +15,7 @@ public class Proyecto01 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Ejemplo de Java Manejado desde GitHub Versionado a traves de Git Creado por danyberna2002@gmail.com");
         
     }
     
